08a96c0762ff35cd70bc2b61cda2a1a8e72b1bfa
Filippo Barcellos <filippobarcellos@Filippos-MacBook-Pro.local>
2024-05-21 11:20:11 +0100

    add apple stuff

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,32 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Universal Links spike test!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Universal Links spike test!!" />
+        <button
+          onClick={"https://main--curious-duckanoo-749871.netlify.app/redirect"}
+        >
+          continue on app
+        </button>
+        <button
+          onClick={
+            "https://main--curious-duckanoo-749871.netlify.app/no-redirect"
+          }
+        >
+          this one won't redirect
+        </button>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
